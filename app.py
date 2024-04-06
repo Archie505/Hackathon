3be3d1a6fc58ac8f5a4a5003703d5c28628e0ff0
Marc De Jesus <131229911+marcdejesus@users.noreply.github.com>
2024-04-06 01:39:15 -0400

    1.1

--- app.py
+++ app.py
@@ -6,13 +6,33 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
-@app.route("/base")
-def base():
-    return render_template("base.html")
-
 @app.route("/browse")
 def browse():
     return render_template("browse.html")
+
+@app.route("/browse/travel")
+def browse_travel():
+    return render_template("browse_travel.html")
+
+@app.route("/browse/tutoring")
+def browse_tutoring():
+    return render_template("browse_tutoring.html")
+
+@app.route("/browse/creatives")
+def browse_creatives():
+    return render_template("browse_creatives.html")
+
+@app.route("/browse/other_services")
+def browse_other_services():
+    return render_template("browse_other_services.html")
+
+@app.route("/makepost")
+def makepost():
+    return render_template("makepost.html")
+
+@app.route("/submitted")
+def submitted():
+    return render_template("submitted.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
